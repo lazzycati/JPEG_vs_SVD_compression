b9ea114a5406fdc741e37e93a8fd4af127b986ad
lazzycati <braunanastasiap2007@gmail.com>
2026-04-20 16:38:43 +0700

    fix architectures and headline announcements

--- basicmatrix.h
+++ basicmatrix.h
@@ -1,21 +1,11 @@
 #include "loadbmp.h"
 #pragma once 
 
-typedef struct {
-    double **data;
-    int rows;
-    int cols;
-} Matrix;
-
-typedef struct {
-    Matrix *U;      // левые сингулярные векторы 
-    double *S;      // сингулярные числа 
-    Matrix *V;      // правые сингулярные векторы 
-} SVD;
+typedef struct Matrix Matrix;
 
 void createMatrix(Matrix *matrix, int rows, int cols);
 void freeMatrix(Matrix *matrix);
 void image_to_matrix(Matrix *matrix, Image *img);
 void matrix_to_image(Matrix *matrix, Image *img);
-void identity_matrix(Matrix *matrix, int N);
-void copy_matrix(Matrix *src, Matrix *dst);
+void identityMatrix(Matrix *matrix, int N);
+void copyMatrix(Matrix *src, Matrix *dst);
