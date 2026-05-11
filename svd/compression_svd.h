0b2ec84324675ea31821af60ed046232ba6bcb83
lazzycati <braunanastasiap2007@gmail.com>
2026-05-11 16:45:04 +0700

    svd теперь многоканальный:)

--- svd/compression_svd.h
+++ svd/compression_svd.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "basicmatrix.h"
 #include "svd.h"
+#include "subdiscretization.h"
+#include "loadbmp.h"
 
-void save_reconstructedmatrix_to_bmp(SVD *svd, int k, int m, int n, char *filename);
-void save_svd_compressed(char *filename, SVD *svd, int k, int m, int n);
-SVD* load_svd_compressed(char *filename, int *m, int *n, int *k);
-void print_compression_stats(char *svd_filename, char *bmp_filename, int m, int n, int k);
-void print_compression_stats_float(char *svd_filename, char *bmp_filename, int m, int n, int k);
-SVD* load_svd_compressed_float(char *filename, int *m, int *n, int *k);
-void save_svd_compressed_float(char *filename, SVD *svd, int k, int m, int n);
+void save_svd_compressed(char *filename, YCbCrSVD *svd, int rY, int rCb, int rCr);
+YCbCrSVD* load_svd_compressed(char *filename, int *rY, int *rCb, int *rCr);
+YCbCrImage420* reconstruct_from_svd(YCbCrSVD *svd, int kY, int kCb, int kCr);
+void print_stats(char *svd_filename, char *bmp_filename, int m, int n, int k);
+void free_ycbcr_svd(YCbCrSVD *svd);
