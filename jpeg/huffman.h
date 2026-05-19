2bd718a1d574a9b50f85654ebe62ce0ddf51b3b7
lazzycati <braunanastasiap2007@gmail.com>
2026-05-20 00:02:28 +0700

    убраны ф-ии работы с потоком и перенесены в папку со вспомогат., т.к. их использует мно-во ф-ий

--- jpeg/huffman.h
+++ jpeg/huffman.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include "jpegtypes.h"
-#include <string.h>
-#include <stdlib.h>
+#include "bitstream.h"
 typedef struct {
     uint8_t bits[17]; // Количество кодов каждой длины 
     uint8_t huffval[256];   // Значения кодов в порядке увеличения длины кода
@@ -20,10 +19,3 @@ void init_tables();
 void generate_codes(HuffmanTable *table);
 int huffman_encode_simb(BitStream *stream, int s, HuffmanTable *table);
 int huffman_decode_simb(BitStream *stream, HuffmanTable *table, int *s);
-//добавляет бит во временный буфер и по необходимости вып. flush, учитывает байт-стаффинг
-void add_bit(BitStream *stream, int bit);
-void write_bits(BitStream *stream, int value, int num_bits);
-//Дописывает недостающие биты (единицами) до полного байта и записывает последний байт.
-void flush(BitStream *stream);
-//Читает бит с потока
-int read_bit(BitStream *stream);
